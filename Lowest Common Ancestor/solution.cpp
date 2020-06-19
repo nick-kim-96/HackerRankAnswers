eaa9fdb5d3d37b6fe4ee5c59f5ea7e48e6da6869
nick-kim-96 <nickthuazn@gmail.com>
2020-06-18 21:15:21 -0700

    fix for solution

--- Lowest Common Ancestor/solution.cpp
+++ Lowest Common Ancestor/solution.cpp
@@ -47,11 +47,11 @@ class Node {
 
         if(root->data < v1 && root->data < v2)
         {
-            return(lca(root->left, v1, v2));
+            return(lca(root->right, v1, v2));
         }
         else if (root->data > v1 && root->data > v2)
         {
-            return(lca(root->right, v1, v2));
+            return(lca(root->left, v1, v2));
         }
         else
         {
